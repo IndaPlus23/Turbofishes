14c9307b23e6e2307ce44072910d89eeb0558c55
Daniel Strömberg <mail@danielst.se>
2023-09-15 16:14:40 +0200

    task 3 rename

---
+++ chess-lib/chess_template/target/debug/deps/chess_template-996bbc58dbe2b893.d
@@ -0,0 +1,7 @@
+/Users/c14/ta/ferrific/rust-task-3/chess_template/target/debug/deps/chess_template-996bbc58dbe2b893.rmeta: src/lib.rs
+
+/Users/c14/ta/ferrific/rust-task-3/chess_template/target/debug/deps/libchess_template-996bbc58dbe2b893.rlib: src/lib.rs
+
+/Users/c14/ta/ferrific/rust-task-3/chess_template/target/debug/deps/chess_template-996bbc58dbe2b893.d: src/lib.rs
+
+src/lib.rs:
