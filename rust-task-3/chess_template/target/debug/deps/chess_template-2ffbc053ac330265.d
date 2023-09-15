80edf1773b3ac0f7c481335f3bfbeffd859aac89
Daniel Strömberg <mail@danielst.se>
2023-09-15 16:16:20 +0200

    removed old task 3

--- rust-task-3/chess_template/target/debug/deps/chess_template-2ffbc053ac330265.d
+++
@@ -1,5 +0,0 @@
-/Users/c14/ta/ferrific/rust-task-3/chess_template/target/debug/deps/chess_template-2ffbc053ac330265: src/lib.rs
-
-/Users/c14/ta/ferrific/rust-task-3/chess_template/target/debug/deps/chess_template-2ffbc053ac330265.d: src/lib.rs
-
-src/lib.rs:
