acc1a5062914327b0b633c071a5d9c54702eda34
Daniel Strömberg <mail@danielst.se>
2024-01-19 11:21:37 +0100

    task 14

---
+++ dynprog-task-14/kattis_template/target/debug/deps/kattis_template-0ed9d43ec27303c9.d
@@ -0,0 +1,5 @@
+/Users/c14/ta/ferrific/dynprog-task-14/kattis_template/target/debug/deps/libkattis_template-0ed9d43ec27303c9.rmeta: src/main.rs
+
+/Users/c14/ta/ferrific/dynprog-task-14/kattis_template/target/debug/deps/kattis_template-0ed9d43ec27303c9.d: src/main.rs
+
+src/main.rs:
---
+++ dynprog-task-14/kattis_template/target/debug/deps/kattis_template-9ef16966542a10bb.d
@@ -0,0 +1,5 @@
+/Users/c14/ta/ferrific/dynprog-task-14/kattis_template/target/debug/deps/libkattis_template-9ef16966542a10bb.rmeta: src/main.rs
+
+/Users/c14/ta/ferrific/dynprog-task-14/kattis_template/target/debug/deps/kattis_template-9ef16966542a10bb.d: src/main.rs
+
+src/main.rs:
